b0ab3421aab75b6f6db045a22479a25949139d6c
ahtesham ahmed <160284482+ahteshamahmed12@users.noreply.github.com>
2025-04-06 23:52:16 +0500

    first

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Lets play a mad libs game ")
+
+noun=input(" Enter a noun : ")
+verb=input(" Enter a verb :")
+adjective = input(" Enter a adjective : ")
+
+print(f"Today, I went to a place {noun} and I saw a {adjective} {noun} that was {verb}ing.")
