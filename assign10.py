31308f69b36852a3c7bc7b028437e34aa3e986dd
ahtesham ahmed <160284482+ahteshamahmed12@users.noreply.github.com>
2025-04-08 16:08:32 +0500

    initial commit

---
+++ assign10.py
@@ -0,0 +1,8 @@
+
+number1= int(input("Please enter an integer to be divided : "))
+number2= int(input("Please enter an integer to be divided by : "))
+
+divide = number1 / number2
+reminder=number1 % number2
+
+print(F"The result of the division is {divide} and reminder is {reminder}")
